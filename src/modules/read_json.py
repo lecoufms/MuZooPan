9e980e871b84d00e34b6add68850acefbbea8278
Wesley Barbosa <34480299+wbs2A@users.noreply.github.com>
2018-04-23 11:27:06 -0400

    Remoção do erro de merge

--- src/modules/read_json.py
+++ src/modules/read_json.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import json
-
-def parseJson(name,dir=''):
-    try:
-        with open(str(dir+name+'.json')) as json_data:
-            d = json.load(json_data)
-            return d[0]
-    except Exception as e:
-        print(e)
-        return {'alternativa e': '', 'Qual o nome do animal?': '', 'Insira o texto para pergunta':''}
-=======
 import json, codecs
 
 def parseJson(name,dir=''):
@@ -23,4 +11,3 @@ def parseJson(name,dir=''):
     except Exception as e:
         print(e)
         return {'habitat': '', 'nome do animal': '', 'reproducao':'', 'caracteristicas':'','curiosidades':''}
->>>>>>> origin/development
