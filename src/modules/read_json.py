51ba0a8dbf606d5c419730c99f8a2c1a00488740
ValdenizeRibeiro <valdenizesilva8@gmail.com>
2018-05-15 15:34:37 -0400

    Update read_json.py

--- src/modules/read_json.py
+++ src/modules/read_json.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> e8483d6ab3f7ff442f35f384c19648894864e04a
 import json, codecs
 
 def parseJson(name,dir=''):
@@ -15,7 +11,3 @@ def parseJson(name,dir=''):
     except Exception as e:
         print(e)
         return {'habitat': '', 'nome do animal': '', 'reproducao':'', 'caracteristicas':'','curiosidades':''}
-<<<<<<< HEAD
-=======
-
->>>>>>> e8483d6ab3f7ff442f35f384c19648894864e04a
